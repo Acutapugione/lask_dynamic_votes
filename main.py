11775a0b74c76f82677d8e44edc18fb639177f38
Acutapugione <acuta.pugione@gmail.com>
2024-09-08 19:26:54 +0300

    filters and macro added

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import random
+from json import dump, load
+from typing import final
 from flask import Flask, render_template
 
 
+def custom_filter(value):
+    return f"{value}s"
+
+
+@final
 class Client(object):
     def __init__(self, app: Flask):
         self.app = app
+        self.app.jinja_env.filters["custom_filter"] = custom_filter
         self.app.add_url_rule("/", view_func=self.index)
-        self.app.add_url_rule("/data", view_func=self.data)
+        self.app.add_url_rule("/students", view_func=self.students)
 
-    def data(self):
-        return {"my little pohny": 123}
+    def students(self):
+        def get_students(filename: str = "data.json") -> list[dict]:
+            with open(filename, "r", encoding="utf-8") as f:
+                return load(f)
+
+        return render_template("list.html", items=get_students())
 
     def index(
         self,
@@ -17,8 +29,10 @@ class Client(object):
             "GET",
         ],
     ):
+        endpoints = [rule.endpoint for rule in self.app.url_map.iter_rules()]
+
         number = random.randint(0, 10000)
-        return render_template("index.html", number=number)
+        return render_template("index.html", number=number, endpoints=endpoints)
 
     def run(self, debug: bool = True):
         self.app.run(debug=debug)
